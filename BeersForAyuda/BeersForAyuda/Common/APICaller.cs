dc0f87f34ecbc09d38b870069e1a185eeb2bec8c
Ahmet Emir KESIM <ahmetemirkesim@gmail.com>
2018-03-06 20:33:33 +0300

    Initial phase of project.

---
+++ BeersForAyuda/BeersForAyuda/Common/APICaller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+using System.Net.Http;
+
+
+namespace BeersForAyuda.Common
+{
+    public static class APICaller
+    {
+        public static string _url = System.Configuration.ConfigurationManager.AppSettings["APIUrl"];
+
+        public static void LoadCacheData()
+        {
+           
+        }
+
+        public static string CreateResponse(string uri)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var builder = new UriBuilder(uri);
+                builder.Port = -1;
+                var query = HttpUtility.ParseQueryString(builder.Query);
+                query["key"] = System.Configuration.ConfigurationManager.AppSettings["APIKey"];
+                builder.Query = query.ToString();
+                string url = builder.ToString();
+                return httpClient.GetStringAsync(new Uri(url)).Result;
+            }
+        }
+    }
+}
